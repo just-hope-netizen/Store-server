32e683a784b86a4778e4aa954e81b9c7e7b423bf
just-hope-netizen <hopepeter007@gmail.com>
2024-04-16 18:42:04 +0100

    Refactor - securityfilterchain

---
+++ src/main/java/com/netizen/storeserver/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.netizen.storeserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+               return http.
+//                       csrf( csrf -> csrf.disable()).
+                       authorizeHttpRequests( request -> request.requestMatchers("api/v1/**")
+                               .permitAll().anyRequest().authenticated()).build();
+    }
+
+}
--- src/main/java/com/netizen/storeserver/controller/StoreController.java
+++ src/main/java/com/netizen/storeserver/controller/StoreController.java
@@ -4,27 +4,25 @@ import com.netizen.storeserver.model.Product;
 import com.netizen.storeserver.service.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Controller
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("api/v1/")
 public class StoreController {
 
     @Autowired
     private StoreService service;
 
-    @GetMapping("/")
+    @GetMapping("products")
     public List<Product> getAllProducts(){
         return service.getAllProducts();
     };
 
-    @GetMapping("/search")
+    @GetMapping("search")
         public List<Product> getProducts(@RequestParam String q) {
       return  service.getProducts(q);
         }
